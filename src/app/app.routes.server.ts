51d9bc0166409eba3e7d934fa25e1ba689eb3b08
James Walsh <walsh-19@ulster.ac.uk>
2026-03-04 17:06:40 +0000

    Disable prerendering and reinforce build budgets

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,14 +2,6 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: 'login',
-    renderMode: RenderMode.Prerender
-  },
-  {
-    path: 'register',
-    renderMode: RenderMode.Prerender
-  },
-  {
     path: '**',
     renderMode: RenderMode.Server
   }
