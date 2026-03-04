311375e6cd471d1ba828af4076de989257cac461
James Walsh <walsh-19@ulster.ac.uk>
2026-03-04 16:55:53 +0000

    Final live URLs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://ctims-backend-james.azurewebsites.net/api' 
+  apiUrl: 'https://ctims-backend-james-cubnbbazahekhpd7.uksouth-01.azurewebsites.net/api' 
 };
